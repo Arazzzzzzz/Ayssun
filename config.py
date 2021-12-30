f2917c11d335ccc39b3761090afbbff1d1fffa22
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-12-30 17:40:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config(object):
     DATABASE = os.environ.get("DB_URI")
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
     SUPPORT = os.environ.get("SUPPORT")
+    BOT_NAME = os.environ.get("bn")
+    BOT_USERNAME = os.environ.get("bn")
